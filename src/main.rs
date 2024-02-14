8b9e40a96649dd897209dccb334d3eb46fed7842
Ahmed Asi <95191621+AchoDev@users.noreply.github.com>
2024-02-14 15:59:11 +0100

    better ui and editing with enter and backspave

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,11 @@ fn main() -> io::Result<()> {
         let mut changed_line = false;
         if let Ok(event) = read() {
             if let Event::Key(key_event) = event {
-                if key_event.kind != KeyEventKind::Press {
+                if key_event.kind != KeyEventKind::Press && !initial {
                     continue;
+                }
+                if initial {
+                    initial = false;
                 }
                 match key_event.code {
                     KeyCode::Down => {
@@ -59,14 +62,28 @@ fn main() -> io::Result<()> {
                         }
                     }
                     KeyCode::Left => {
+                        if current_char == 0 {
+                            continue;
+                        }
                         current_char -= 1;
                         changed_line = true;
+                        clear_all()?;
                     }
                     KeyCode::Enter => {
                         changed_line = true;
-                        current_char = 0;
                         current_line += 1;
                         lines.insert(current_line, String::new());
+
+                        if current_char != 0 && current_char < lines[current_line - 1].len() {
+                            lines[current_line] = lines[current_line - 1]
+                                [current_char..lines[current_line - 1].len()]
+                                .to_string();
+
+                            lines[current_line - 1] =
+                                lines[current_line - 1][0..current_char].to_string();
+                        }
+
+                        current_char = 0;
                         clear_all()?;
                     }
 
@@ -77,17 +94,26 @@ fn main() -> io::Result<()> {
                         if current_char >= lines[current_line].len() {
                             lines[current_line].push(c);
                         } else {
-                            lines[current_line] = lines[current_line][0..current_char].to_string()
+                            lines[current_line] = lines[current_line][0..current_char - 1]
+                                .to_string()
                                 + &c.to_string()
-                                + &lines[current_line][current_char + 1..lines[current_line].len()]
+                                + &lines[current_line][current_char - 1..lines[current_line].len()]
                         }
                     }
                     KeyCode::Backspace => {
-                        if lines[current_line].len() == 0 {
-                            continue;
-                        }
-                        changed_line = true;
-                        if current_char >= lines[current_line].len() {
+                        if current_char == 0 {
+                            if current_line == 0 {
+                                continue;
+                            }
+
+                            let copied_line = lines[current_line].clone();
+                            lines[current_line - 1].push_str(&copied_line);
+                            lines.remove(current_line);
+
+                            current_line -= 1;
+                            current_char = lines[current_line].len() - copied_line.len();
+                            current_char += 1;
+                        } else if current_char >= lines[current_line].len() {
                             lines[current_line].pop();
                             // clear_all()?;
                         } else {
@@ -96,12 +122,12 @@ fn main() -> io::Result<()> {
                                 + &lines[current_line][current_char..lines[current_line].len()]
                         }
                         current_char -= 1;
-
-                        execute!(
-                            io::stdout(),
-                            MoveTo(current_char as u16, current_line as u16)
-                        )?;
+                        changed_line = true;
                         clear_all()?;
+                        // execute!(
+                        //     io::stdout(),
+                        //     MoveTo(current_char as u16, current_line as u16)
+                        // )?;
                     }
 
                     _ => {}
@@ -110,12 +136,12 @@ fn main() -> io::Result<()> {
         }
 
         execute!(io::stdout(), MoveTo(0, 0))?;
-
-        if initial {
-            initial = false;
-        } else if !changed_line {
+        clear_screen()?;
+        if !changed_line {
             continue;
         }
+
+        // println!("{} {}", current_char, lines[current_line].len());
 
         let char = match current_char == lines[current_line].len() {
             true => ' '.on_white(),
@@ -126,8 +152,11 @@ fn main() -> io::Result<()> {
                 .on_white(),
         };
 
+        println!("{}", "Pico - AchoDev".dark_blue());
+        println!("{}", "----| test_file.txt".dark_grey());
+
         for (i, line) in lines.iter().enumerate() {
-            let mut start: &str = "";
+            let start: &str;
             let mut end: &str = "";
 
             if current_line == i {
@@ -139,7 +168,16 @@ fn main() -> io::Result<()> {
                 start = &line
             }
 
-            clear_screen()?;
+            print!("{}", (i + 1).to_string().dark_grey());
+            let mut spacer_length: i16 = 3;
+            spacer_length -= (i + 1).to_string().len() as i16;
+
+            while spacer_length >= 0 {
+                print!(" ");
+                spacer_length -= 1;
+            }
+
+            print!("{}", "| ".dark_grey());
             print!("{}", start);
 
             if current_line == i {
@@ -151,8 +189,17 @@ fn main() -> io::Result<()> {
             // print!("{}", &lines[current_line]);
 
             println!("");
-            io::stdout().flush()?;
         }
+        println!("{}", "----|".dark_grey());
+        print!("Line: {} Char: {}", current_line + 1, current_char);
+        println!("\n\n{}", "Press ALT to enter Edit Mode".blue());
+        println!("{}", "Press F1 to enter Menu Mode".blue());
+        println!("{}", "Press ESC to exit Pico".blue());
+
+        // println!("Debug info");
+        // println!("{:?}", lines);
+
+        io::stdout().flush()?;
     }
 
     disable_raw_mode()?;
