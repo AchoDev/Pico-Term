036c349abd106acaa32e0002743fe757a982b286
Ahmed Asi <95191621+AchoDev@users.noreply.github.com>
2024-04-23 13:03:56 +0200

    started work on formatting

--- src/bin/regex_test.rs
+++ src/bin/regex_test.rs
@@ -1,8 +1,9 @@
 use regex::Regex;
 
 fn main() {
-    let re =
+    let keywords =
         Regex::new(r"\b(func|var|struct|if|elseif|else|static|return|true|false|null)\b").unwrap();
+    let types = Regex::new(r"\bstruct\s+(\w+)").unwrap();
     let code = "
         func main() {
             return 'hello'
@@ -13,9 +14,15 @@ fn main() {
         }
     ";
 
-    for caps in re.captures_iter(&code) {
+    for caps in keywords.captures_iter(&code) {
         let mat = caps.get(0).unwrap();
-        print!("One match found: {} ", mat.as_str());
+        print!("Keyword: {} ", mat.as_str());
+        print!("Start: {} ", mat.start());
+        print!("End: {}\n", mat.end());
+    }
+    for caps in types.captures_iter(&code) {
+        let mat = caps.get(1).unwrap();
+        print!("Type: {} ", mat.as_str());
         print!("Start: {} ", mat.start());
         print!("End: {}\n", mat.end());
     }
